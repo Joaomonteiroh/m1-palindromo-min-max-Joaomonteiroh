11f7085f070d56f37bb96b1d20ab2b1476a969e8
Joaomonteiroh <joaomonteirov8@gmail.com>
2024-07-25 19:20:14 -0300

    Código adicionado

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+
+let text = str.toLowerCase().replaceAll(" ","");
+
+    for (let i = 0; i < text.length; i++) {
+
+        if (text[i] == text[text.length - 1 - i]) {
+            return true;
+        } else {
+            return false;
+        }
+    }      
 }
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+
+let min = arr[0];
+let max = arr[0];
+    
+    for (let i = 1; i < arr.length; i++) {
+    
+        if (arr[i] < min) {
+            min = arr[i];
+    
+        } else if (arr[i] > max) {
+            max = arr[i];
+        }
+      }
+    
+return [min, max];
+    
 }
